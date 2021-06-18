7e1a376ccdb969fb3950ca28ee4c5518457c1a9d
pde-grif <pde-grif@student.42sp.org.br>
2021-06-18 18:51:15 +0000

    C01 - 100%

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,23 @@
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			if (tab[j] > tab[i])
+			{
+				temp = tab[j];
+				tab[j] = tab[i];
+				tab[i] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
